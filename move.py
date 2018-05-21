521a0a246655df65da98249782b7fa394cc79547
jorczyk <smajorc@gmail.comgit config --global user.email smajorc@gmail.comgit config --global user.email smajorc@gmail.com>
2018-05-21 22:20:15 +0200

    done & working properly

--- move.py
+++ move.py
@@ -4,43 +4,7 @@ import os
 import shutil
 from glob import glob
 from pathlib import Path
-
-# N = 3  # the number of files in seach subfolder folder
-N = 5  # ratio train/test samples
-
-
-def move_files(abs_dirname):
-    """Move files into subdirectories."""
-
-    files = [os.path.join(abs_dirname, f) for f in os.listdir(abs_dirname)]
-
-    i = 0
-    curr_subdir = None
-
-    for f in files:
-        if i % N == 0:
-            subdir_name = os.path.join(abs_dirname, 'test\\')
-            if not os.path.exists(subdir_name):
-                os.mkdir(subdir_name)
-            curr_subdir = subdir_name
-        else:
-            subdir_name = os.path.join(abs_dirname, 'train')
-            if not os.path.exists(subdir_name):
-                os.mkdir(subdir_name)
-            curr_subdir = subdir_name
-
-            # for f in files:
-            #     # create new subdir if necessary
-            #     if i % N == 0:
-            #         subdir_name = os.path.join(abs_dirname, '{0:f}'.format(i / N + 1))
-            #         os.mkdir(subdir_name)
-            #         curr_subdir = subdir_name
-
-        # move file to current dir
-        f_base = os.path.basename(f)
-        shutil.move(f, os.path.join(subdir_name, f_base))
-        i += 1
-
+import time
 
 def parse_args():
     """Parse command line arguments passed to script invocation."""
@@ -51,17 +15,11 @@ def parse_args():
 
     return parser.parse_args()
 
-
 def get_all_subfoders(src_dir):
     if not os.path.exists(src_dir):
         raise Exception('Directory does not exist ({0}).'.format(src_dir))
     list_of_dirs = glob(src_dir + "/*/")
     return list_of_dirs
-
-
-# def get_basename(dir_name):
-#     path = os.path.dirname(dir_name)
-#     return os.path.basename(path)
 
 def get_head(path):
     return str(Path(path).parent)
@@ -69,37 +27,69 @@ def get_head(path):
 def rename_dir(old_dir_name, index):
     head = get_head(old_dir_name)
     new_name = head + "\\" + str(index)
-    os.rename(old_dir_name, new_name)
+    if not os.path.exists(new_name):
+        os.rename(old_dir_name, new_name)
     return new_name
 
+def rename_folders(src_dir):
+    for index, directory in enumerate(get_all_subfoders(src_dir)):
+        print("renaming folder " + rename_dir(os.path.dirname(directory), index))
+
+def split_train_test_sets(src_dir):
+    N = 5  # ratio train/test samples
+
+    rename_folders(src_dir)
+    print('~~RENAMING DONE~~')
+    subfolders = get_all_subfoders(src_dir)
+    parent = get_head(src_dir)
+
+    subdir_test_name = os.path.join(parent, 'test\\')
+    if not os.path.exists(subdir_test_name):
+        os.mkdir(subdir_test_name)
+        print(subdir_test_name + " folder created")
+    subdir_train_name = os.path.join(parent, 'train\\')
+    if not os.path.exists(subdir_train_name):
+        os.mkdir(subdir_train_name)
+        print(subdir_train_name + " folder created")
+
+    print("~~DONE CREATING TRAIN/TEST FOLDERS")
+
+    i = 1
+    for folder in subfolders:
+        # print(folder)
+        files = [os.path.join(folder, f) for f in os.listdir(folder)]
+        for file in files:
+            f_base = os.path.basename(file)
+            item = os.path.basename(get_head(file))
+            if i % N == 0:
+                target_dir = subdir_test_name
+            else:
+                target_dir = subdir_train_name
+
+            target = target_dir + item + '\\'
+            if not os.path.exists(target):
+                print("creating folder: " + target)
+                os.mkdir(target_dir + item + '\\')
+
+            if not os.path.isfile(target + f_base):
+                try:
+                    shutil.copy(file, target + f_base)
+                except:
+                    raise IOError('An error occured')
+                print('\"' + file + '\"' + " -> " + '\"' + target + f_base + '\"' + " copy done")
+            i += 1
+    print("~~DONE SUCCESFULLY~~")
 
 def main():
-    """Module's main entry point (zopectl.command)."""
     args = parse_args()
     src_dir = args.src_dir
 
     if not os.path.exists(src_dir):
         raise Exception('Directory does not exist ({0}).'.format(src_dir))
 
-    move_files(os.path.abspath(src_dir))
-
-def rename_folders(src_dir):
-    for index, directory in enumerate(get_all_subfoders(src_dir)):
-        print(rename_dir(os.path.dirname(directory), index))
+    split_train_test_sets(os.path.abspath(src_dir))
 
 if __name__ == '__main__':
-    args = parse_args()
-    src_dir = args.src_dir
-    subfolders = get_all_subfoders(src_dir)
-    parent = get_head(src_dir)
-
-    subdir_name = os.path.join(parent, 'test\\')
-    if not os.path.exists(subdir_name):
-        os.mkdir(subdir_name)
-    subdir_name = os.path.join(parent, 'train\\')
-    if not os.path.exists(subdir_name):
-        os.mkdir(subdir_name)
-
-    for folder in subfolders:
-        print(folder)
-        # main()
+    start_time = time.time()
+    main()
+    print("--- %s seconds elapsed ---" % (time.time() - start_time))
